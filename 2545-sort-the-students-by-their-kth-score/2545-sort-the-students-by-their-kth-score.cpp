60f57df45421e52f8c4972711db9a0d6711786c4
vaibhav <99118025+VaibhavSr007@users.noreply.github.com>
2023-01-22 19:32:58 +0530

    Time: 90 ms (69.23%), Space: 28 MB (15.38%) - LeetHub

---
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    static bool cmp(pair<int,int> a, pair<int,int> b){
+        if(a.second > b.second){
+            return true;
+        }
+        return false;
+    }
+    vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
+        map<int,int> mp;
+        for(int i=0; i<score.size(); i++){
+            mp[i] = score[i][k];
+        }
+        vector<pair<int,int>> v;
+        for(auto i:mp){
+            v.push_back({i.first,i.second});
+        }
+        sort(begin(v),end(v),cmp);
+        for(auto i:v){
+            cout<<i.first<<":"<<i.second<<" ";
+        }
+        vector<vector<int>> ans;
+        for(auto i:v){
+            ans.push_back(score[i.first]);
+        }
+        return ans;
+    }
+};
