f9ac06182b3cf503d3c7bc7adf5ba025e67a0e3a
vaibhav <99118025+VaibhavSr007@users.noreply.github.com>
2023-01-22 19:29:40 +0530

    Time: 3 ms (10.00%), Space: 6 MB (30.00%) - LeetHub

---
+++ 2543-check-if-point-is-reachable/2543-check-if-point-is-reachable.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int gcd(int a, int b){
+        
+        if(a==0){
+            return b;
+        }
+        return gcd(b%a,a);
+    }
+    bool isReachable(int targetX, int targetY) {
+        
+        while(targetX%2==0){
+            targetX /=2;
+        }
+        while(targetY%2==0){
+            targetY /=2;
+        }
+        
+        return gcd(targetX,targetY)==1;
+    }
+};
