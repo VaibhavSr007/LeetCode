a140d52c794943760ab2d0a63244dc7809b9f16c
vaibhav <99118025+VaibhavSr007@users.noreply.github.com>
2023-02-23 12:23:33 +0530

    Time: 145 ms (53.07%), Space: 61.3 MB (68.04%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int a=0;
+        // int a=0;
         // sort(nums.begin(), nums.end());
         // for(int i=0; i<nums.size(); i++){
         //     if(nums[i] == nums[i+1]){
@@ -12,14 +12,11 @@ public:
         // return a;
         
         
-        int n = nums.size();
-        
+//        int n = nums.size();       
 //         vector<int> v(n,0);
-        
 //         for(int i=0; i<nums.size(); i++){
-//             v[nums[i]]++;
+//             v[nums[i]]++;                           // extra space of freq array
 //         }
-        
 //         for(int i=1; i<v.size(); i++){
 //             if(v[i]>1){
 //                 a = i;
@@ -27,15 +24,35 @@ public:
 //         }
 //         return a;
         
-        set<int> s;
-        for(auto i:nums){
-            if(s.find(i)==s.end()){
-                s.insert(i);
+        
+        // set<int> s;
+        // for(auto i:nums){
+        //     if(s.find(i)==s.end()){                  // extra space of set
+        //         s.insert(i);
+        //     }
+        //     else{
+        //         return i;
+        //     }
+        // }
+        // return 0;
+        
+        int s = 1, e=nums.size()-1; 
+        
+        while(s<=e){
+            int m = s + (e-s)/2;
+            int cnt=0;
+            for(auto i:nums){
+                if(i<=m){
+                    cnt++;
+                }
+            }                       // most optimized time 0(logn) space 0(1)
+            if(cnt>m){
+                e = m-1;
             }
             else{
-                return i;
+                s = m+1;
             }
         }
-        return 0;
+         return s;
     }
 };
