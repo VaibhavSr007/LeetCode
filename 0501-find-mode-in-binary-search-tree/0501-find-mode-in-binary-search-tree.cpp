e53bc91f76785fbb26a671b1f01f6ce78b5997fb
vaibhav <99118025+VaibhavSr007@users.noreply.github.com>
2023-02-28 16:50:05 +0530

    Time: 29 ms (41.03%), Space: 25.6 MB (37.83%) - LeetHub

---
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root, map<int,int>& mp){
+        if(!root){
+            return ;
+        }
+        
+        inorder(root->left,mp);
+        mp[root->val]++;
+        inorder(root->right,mp);
+    }
+    vector<int> findMode(TreeNode* root) {
+        map<int,int> mp;
+        inorder(root,mp);
+        int maxi = INT_MIN;
+        for(auto i:mp){
+            if(i.second > maxi){
+                maxi = i.second;
+            }
+        }
+        vector<int> ans;
+        for(auto i:mp){
+            if(i.second==maxi){
+                ans.push_back(i.first);
+            }
+        }
+        return ans;
+    }
+};
