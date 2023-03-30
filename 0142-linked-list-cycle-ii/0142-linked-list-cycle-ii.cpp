e428348570fea2d27f3f15cb9efe2717b919eb89
vaibhav <99118025+VaibhavSr007@users.noreply.github.com>
2023-03-30 13:01:13 +0530

    Time: 18 ms (12.08%), Space: 10 MB (9.25%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -10,32 +10,32 @@ class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
         
-        if(head == NULL){ // if list is already empty 
-            return NULL;
-        }
+//         if(head == NULL){ // if list is already empty 
+//             return NULL;
+//         }
         
-        ListNode *slow = head;
-        ListNode *fast = head;
+//         ListNode *slow = head;
+//         ListNode *fast = head;
     
-        while(fast != NULL && fast->next != NULL){//gets intersection node if loop present else
-            slow = slow->next;                    // else takes the fast index to NULL
-            fast = fast->next->next;
-            if(slow == fast){
-                break;
-            }
-        }
-        if(fast == NULL || fast->next == NULL){ // if fast reahes null so no loop
-            return NULL;
-        }
+//         while(fast != NULL && fast->next != NULL){//gets intersection node if loop present else
+//             slow = slow->next;                    // else takes the fast index to NULL
+//             fast = fast->next->next;
+//             if(slow == fast){
+//                 break;
+//             }
+//         }
+//         if(fast == NULL || fast->next == NULL){ // if fast reahes null so no loop
+//             return NULL;
+//         }
     
-        slow = head; 
+//         slow = head; 
     
-        while(slow != fast){ //start new node from head and fast from where it was, move both with 
-            slow = slow->next; // same pace they will intersect at start index
-            fast = fast->next;
-        }
+//         while(slow != fast){ //start new node from head and fast from where it was, move both with 
+//             slow = slow->next; // same pace they will intersect at start index
+//             fast = fast->next;
+//         }
         
-        return fast;
+        // return fast;
         
         
         // ********** FOR REMOVING LOOP ************
@@ -44,5 +44,18 @@ public:
         // }
         // slow->next = NULL; //so slow reached last node of loop or (prev of start(fast)) n we make it null so loop gets removed
         // return head;
+        
+        map<ListNode*, int> mp;
+        ListNode* cur = head;
+        while(cur){
+            if(mp.find(cur) != mp.end()){        // map or set approach
+                // cout<<cur->val<<" ";
+                return cur;
+            }
+            mp[cur] = cur->val;
+            cur = cur->next;
+        }
+        
+        return NULL;
     }
 };
