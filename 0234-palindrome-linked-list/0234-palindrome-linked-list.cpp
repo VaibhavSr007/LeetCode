081a75f335e908f6212df6306fda136ee9e0e79d
vaibhav <99118025+VaibhavSr007@users.noreply.github.com>
2023-03-30 12:00:03 +0530

    Time: 336 ms (5.71%), Space: 148.6 MB (6.22%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,97 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    
+    bool isPalindrome(ListNode* head) {
+//         string s;
+//         ListNode *temp = head;
+//         while(temp!=NULL){
+//             s.push_back(temp->val+'0');
+//             temp = temp->next;
+//         }
+//         string rev = s;                     approach using string
+//         reverse(rev.begin(),rev.end());
+        
+//         if(rev == s){
+//             return true;
+//         }
+//         return false;
+        
+//         ListNode *cur = head;
+//         ListNode *forw = NULL;
+//         ListNode *slow = head;
+//         ListNode *fast = head;
+        
+//         if(head==NULL || head->next==NULL){
+//             return true;
+//         }
+        
+//         while(fast != NULL && fast->next!=NULL){
+//             slow = slow->next;
+//             fast = fast->next->next;
+//         }
+        
+//         ListNode *mid = slow->next;
+//         slow->next = NULL;
+        
+//         while(mid!=NULL){
+//             forw = mid->next;
+//             mid->next = slow;
+//             slow = mid;
+//             mid = forw;
+//         }
+        
+//         while(head!=slow && slow!=NULL){
+//             if(head->val != slow->val){
+//                 return false;
+//             }
+//             slow = slow->next;
+//             head = head->next;
+//         }
+        
+//         return true;
+        
+        ListNode* prev = NULL;
+        ListNode* next = NULL;
+        ListNode* cur = head;
+        ListNode* ans = new ListNode(-1);
+        ListNode* copy = ans;
+        
+        while(cur){
+            ListNode* add = new ListNode(cur->val);
+            copy->next = add;
+            copy = copy->next;
+            cur = cur->next;
+        }
+        
+        cur = head;
+        ans = ans->next;
+        copy = ans;
+        while(copy){
+            next = copy->next;
+            copy->next = prev;
+            prev = copy;
+            copy = next;
+        }
+        
+        ans = prev;
+        while(ans){
+            if(ans->val != head->val){
+                return false;
+            }
+            ans = ans->next;
+            head = head->next;
+        }
+        
+        return true; 
+    }
+};
