aaf97b07cde356da932677aadfeb77446c7eae1e
vaibhav <99118025+VaibhavSr007@users.noreply.github.com>
2023-04-07 09:34:25 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (43.50%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -9,14 +9,23 @@ public:
 //             }
 //         }
         
-            for(int i=0; i<matrix.size(); i++){
-                for(int j=0; j<i; j++){              // taking transpose of matrix
-                    swap(matrix[i][j],matrix[j][i]);
-                }
-            }          // this is more space optimized approach no xtra space needed
+//             for(int i=0; i<matrix.size(); i++){
+//                 for(int j=0; j<i; j++){              // taking transpose of matrix
+//                     swap(matrix[i][j],matrix[j][i]);
+//                 }
+//             }          // this is more space optimized approach no xtra space needed
         
-            for(int i=0; i<matrix.size(); i++){
-                reverse(matrix[i].begin(),matrix[i].end());  // reverse each row of                                                                matrix to get desired ans
+//             for(int i=0; i<matrix.size(); i++){
+//                 reverse(matrix[i].begin(),matrix[i].end());  // reverse each row of                                                                matrix to get desired ans
+//             }
+        
+        for(int i=0; i<matrix.size(); i++){
+            for(int j=0; j<i; j++){
+                swap(matrix[i][j],matrix[j][i]);
             }
+        }
+        for(int i=0; i<matrix.size(); i++){
+            reverse(begin(matrix[i]),end(matrix[i]));
+        }
     }
 };
