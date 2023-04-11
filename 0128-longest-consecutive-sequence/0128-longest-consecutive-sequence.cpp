8730e8882b0aa8c3a32b1cabd02708c1c27afb2b
vaibhav <99118025+VaibhavSr007@users.noreply.github.com>
2023-04-11 15:23:00 +0530

    Time: 2118 ms (11.57%), Space: 51 MB (36.03%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-//         int count =1;
-//         vector<int> v; 
-//         int ans=1;
-//         if(nums.size()<=1){
-//             return nums.size();
-//         }
-        
-//         sort(nums.begin(),nums.end());
-//         nums.erase(unique(nums.begin(),nums.end()),nums.end());
-//         for(int i=0; i<nums.size()-1; i++){
-//             if(nums[i]+1==nums[i+1]){
-//                 count++;
-//             }
-//             else{
-//                 count =1;
-//             }
-//             ans = max(ans,count);
-//         }
-        
-//         return ans;
+        // sort(nums.begin(),nums.end());
+        // int ans = 0;
+        // int cnt = 0;
+        // if(nums.size() < 1){ return 0;}
+        // for(int i=0; i<nums.size()-1; i++){
+        //     if(nums[i]+1 == nums[i+1]){
+        //         cnt++;
+        //     }
+        //     else if(nums[i] == nums[i+1]){
+        //         continue;
+        //     }
+        //     else{
+        //         ans = max(ans,cnt+1);
+        //         cnt = 0;
+        //     }
+        // }
+        // ans = max(ans,cnt+1);
+        // return ans;
         
 //         unordered_map<int,int> s;
 //         int ans = 0;
@@ -45,23 +43,22 @@ public:
 //         }
 //         return ans;
         
-        sort(nums.begin(),nums.end());
+        set<int> s;
+        for(auto i:nums){
+            s.insert(i);
+        }
         int ans = 0;
-        int cnt = 0;
-        if(nums.size() < 1){ return 0;}
-        for(int i=0; i<nums.size()-1; i++){
-            if(nums[i]+1 == nums[i+1]){
-                cnt++;
-            }
-            else if(nums[i] == nums[i+1]){
-                continue;
-            }
-            else{
-                ans = max(ans,cnt+1);
-                cnt = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(s.find(nums[i]-1) == s.end()){ // find a number smallest in its sequence
+                int cnt = 1;
+                int val = nums[i];
+                while(s.find(val+1) != s.end()){
+                    cnt++;
+                    val += 1;
+                }
+                ans = max(ans,cnt);
             }
         }
-        ans = max(ans,cnt+1);
         return ans;
     }
 };
