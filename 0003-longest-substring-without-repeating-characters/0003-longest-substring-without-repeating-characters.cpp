72238de3f9ff89deb89f7c2fa22da2f985fdf7f7
vaibhav <99118025+VaibhavSr007@users.noreply.github.com>
2023-04-13 12:10:13 +0530

    Time: 20 ms (60.74%), Space: 8.5 MB (54.71%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -7,24 +7,24 @@ public:
         return x;
     }
     int lengthOfLongestSubstring(string s) {
-        string ans = "";
-        int max_len = 0;
-        for(int j=0; j<s.length(); j++){
-            ans = "";
-            for(int i=j; i<s.length(); i++){
-                if(ans.find(s[i]) == string::npos){
-                    ans.push_back(s[i]);
-                }
-                else if(ans.find(s[i]) != string::npos){
-                    break;
-                }
-            }
-            if(max_len<ans.length()){
-                max_len = ans.length();
-            }
-        }
+//         string ans = "";
+//         int max_len = 0;
+//         for(int j=0; j<s.length(); j++){
+//             ans = "";
+//             for(int i=j; i<s.length(); i++){
+//                 if(ans.find(s[i]) == string::npos){
+//                     ans.push_back(s[i]);
+//                 }
+//                 else if(ans.find(s[i]) != string::npos){
+//                     break;
+//                 }
+//             }
+//             if(max_len<ans.length()){
+//                 max_len = ans.length();
+//             }
+//         }
         
-        return max_len;
+//         return max_len;
         
         
 //         int len = 0;
@@ -43,25 +43,18 @@ public:
 //         }
 //         return len;
         
-//         if(s.length()==0){return 0;}
-//         int ans = 1;
-//         map<char,int> mp;
-//         int i=0, st = 0;
-//         while(i<s.length()){
-//             if(mp.find(s[i]) != mp.end()){
-//                 ans = max(ans, i-st);
-//                 st = mp[s[i]]+1;
-//                 mp[s[i]] = i;
-//             }
-//             else{
-//                 mp[s[i]] = i;
-//             }
-            
-//             if(i==s.length()-1){
-//                 ans = max(ans,i-st+1);
-//             }
-//             i++;
-//         }
-//         return ans;
+        if(s.length()==0){return 0;}
+        int ans = 1;
+        map<char,int> mp;
+        int i=0, st = 0;
+        while(i<s.length()){
+            if(mp.find(s[i]) != mp.end()){
+                st = max(st,mp[s[i]]+1);
+            }
+            ans = max(ans, i-st+1);
+            mp[s[i]] = i;
+            i++;
+        }
+        return ans;
     }
 };
