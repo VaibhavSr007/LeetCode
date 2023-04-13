7081dc82332e2350e6caf80052693431317e3b71
vaibhav <99118025+VaibhavSr007@users.noreply.github.com>
2023-04-13 12:00:14 +0530

    Time: 82 ms (15.73%), Space: 6.9 MB (97.72%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    int max(int x, int y){
+        if(x < y){
+            return y;
+        }
+        return x;
+    }
+    int lengthOfLongestSubstring(string s) {
+        string ans = "";
+        int max_len = 0;
+        for(int j=0; j<s.length(); j++){
+            ans = "";
+            for(int i=j; i<s.length(); i++){
+                if(ans.find(s[i]) == string::npos){
+                    ans.push_back(s[i]);
+                }
+                else if(ans.find(s[i]) != string::npos){
+                    break;
+                }
+            }
+            if(max_len<ans.length()){
+                max_len = ans.length();
+            }
+        }
+        
+        return max_len;
+        
+        
+//         int len = 0;
+//         int l = 0;
+//         int r = 0;
+//         map<char,int> mp;
+        
+//         while(r<s.length()){
+//             if(mp.find(s[r]) != mp.end()){
+//                 l = max(l, mp[s[r]]+1);
+//             }
+            
+//             mp[s[r]] = r;
+//             len  = max(len, r-l+1);
+//             r++;
+//         }
+//         return len;
+        
+//         if(s.length()==0){return 0;}
+//         int ans = 1;
+//         map<char,int> mp;
+//         int i=0, st = 0;
+//         while(i<s.length()){
+//             if(mp.find(s[i]) != mp.end()){
+//                 ans = max(ans, i-st);
+//                 st = mp[s[i]]+1;
+//                 mp[s[i]] = i;
+//             }
+//             else{
+//                 mp[s[i]] = i;
+//             }
+            
+//             if(i==s.length()-1){
+//                 ans = max(ans,i-st+1);
+//             }
+//             i++;
+//         }
+//         return ans;
+    }
+};
