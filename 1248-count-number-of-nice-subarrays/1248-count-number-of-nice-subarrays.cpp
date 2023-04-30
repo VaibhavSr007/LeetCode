0854391e5c9090b43703d4841d41d4669ff86dbb
vaibhav <99118025+VaibhavSr007@users.noreply.github.com>
2023-05-01 00:30:40 +0530

    Time: 378 ms (6.32%), Space: 93.3 MB (18.43%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        int cnt = 0;
-        int ans = 0;
+        // int cnt = 0;
+        // int ans = 0;
         
 //         for(int i=0; i<nums.size(); i++){
 //             int cnt = 0;
@@ -20,28 +20,43 @@ public:
 //             }
 //         }
         
-        int i=0, j=0, temp=0;
-        while(j<nums.size()){
-            if(nums[j]%2 != 0){
-                cnt++;
-                temp  = 0;
+//         int i=0, j=0, temp=0;
+//         while(j<nums.size()){
+//             if(nums[j]%2 != 0){
+//                 cnt++;
+//                 temp  = 0;
+//             }
+            
+//             j++;
+            
+//             while(cnt == k){
+//                 if(nums[i]%2 != 0){
+//                     cnt--;
+//                 }
+//                 i++;
+//                 cout<<i<<" ";
+//                 temp++;
+//             }
+//             ans += temp;
+            
+//             // cout<<cnt<<" ";
+//         }
+//         cout<<endl;
+//         return ans;
+        
+        map<int,int> mp;
+        int sum = 0, ans = 0;
+        for(int i=0; i<nums.size(); i++){
+            sum += nums[i]%2;
+            if(sum == k){
+                ans++;
             }
             
-            j++;
-            
-            while(cnt == k){
-                if(nums[i]%2 != 0){
-                    cnt--;
-                }
-                i++;
-                cout<<i<<" ";
-                temp++;
+            if(mp.find(sum-k) != mp.end()){
+                ans += mp[sum-k];
             }
-            ans += temp;
-            
-            // cout<<cnt<<" ";
+            mp[sum]++;
         }
-        cout<<endl;
         return ans;
     }
 };
