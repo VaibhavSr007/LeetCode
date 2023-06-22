872bf5a36ee9db423ea41139e2c7b6f4f7bb9990
Vaibhav Srivastava <99118025+VaibhavSr007@users.noreply.github.com>
2023-06-22 16:53:14 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (22.81%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,59 +1,27 @@
 class Solution {
 public:
     
-    void solve(int x, int y, vector<vector<int>>& vis, int n, int m, long long& ans, string op){
-        if(x == n-1 && y == m-1){
-            ans += 1;
-            return ;
+    int solve(int i, int j, int n, int m, vector<vector<int>> &dp){
+        if(i >= n || j >= m){
+            return 0;
+        }
+        if(i == n-1 && j == m-1){
+            return 1;
         }
         
-        vis[x][y] = 1;
-        
-        int new_x = x+1;
-        int new_y = y;
-        if(new_x>=0 && new_x<n && new_y>=0 && new_y<m && vis[new_x][new_y]==0){
-            op.push_back('D');
-            solve(new_x,new_y,vis,n,m,ans,op);
-            op.pop_back();
+        if(dp[i][j] != -1){
+            return dp[i][j];
         }
         
-        new_x = x;
-        new_y = y+1;
-        if(new_x>=0 && new_x<n && new_y>=0 && new_y<m && vis[new_x][new_y]==0){
-            op.push_back('R');
-            solve(new_x,new_y,vis,n,m,ans,op);
-            op.pop_back();
-        }
+        int r = solve(i, j+1, n, m, dp);
+        int d = solve(i+1, j, n, m, dp);
         
-        vis[x][y] = 0;
+        dp[i][j] = r + d;
+        return r + d;
     }
     
-    long long int uniquePaths(int n, int m) {
-        
-        // long long int total_paths = m+n-2; //total direction of movement in right(m-1) n down(n-1) so tot m+n-2
-        // double res = 1;
-        // int x = m-1;
-        // for(int i=1; i<m; i++){ // total combinations of right or down directions among total paths
-        //     res *= (total_paths -x + i);  // tot C tot_down or tot C tot_right
-        //     res /= (i);
-        // }
-        // return int(res);
-        
-        
-        // int x = 0, y=0;
-        // long long ans=0;
-        // vector<vector<int>> vis(n,vector<int> (m,0));
-        // string op = "";
-        // solve(x,y,vis,n,m, ans, op);
-        // return ans;
-        
-        int tot = n+m-2;
-        int r = n-1;
-        long long res = 1;
-        for(int i=1; i<=r; i++){
-            res *= long(tot - r + i);
-            res /= i;
-        }
-        return res;
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n,-1));
+        return solve(0,0,m,n,dp);
     }
 };
