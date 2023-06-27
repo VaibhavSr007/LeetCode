fb410c94182996578bce829bb9f218297525acb7
Vaibhav Srivastava <99118025+VaibhavSr007@users.noreply.github.com>
2023-06-27 11:32:30 +0530

    Time: 16 ms (5.05%), Space: 14.3 MB (5.05%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,38 +1,22 @@
 class Solution {
 public:
-    void solve(int k, int n, set<vector<int>> & ans, vector<int> op,int idx, int sum){
-        
-        if(sum > n){
-            return ;
-        }
-        
-        if(op.size()==k){
-            if(sum==n){
-                ans.insert(op);
+    void solve(int i, int sum, vector<int> op, int n, vector<vector<int>> &ans){
+        if(i > 9){
+            if(sum == 0 && n==op.size()){
+                ans.push_back(op);
             }
             return ;
         }
         
-        for(int i=idx; i<=9; i++){
-            op.push_back(i);
-            sum += i;
-            solve(k,n,ans,op,i+1,sum);
-            sum -= i;
-            op.pop_back();
-            solve(k,n,ans,op,i+1,sum);
-        }
-        
+        op.push_back(i);
+        solve(i+1, sum - i, op, n, ans);
+        op.pop_back();
+        solve(i+1, sum, op, n, ans);
         
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>> res;
-        set<vector<int>> ans;
-        vector<int> op;
-        int idx = 1, sum=0;
-        solve(k,n,ans,op,idx,sum);
-        for(auto i:ans){
-            res.push_back(i);
-        }
-        return res;
-    } 
+        vector<vector<int>> ans;
+        solve(1, n, {}, k, ans);
+        return ans;
+    }
 };
