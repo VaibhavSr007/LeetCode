5bf73c22b0ec9c26e1851e5ba447b6e2cfdfca2b
Vaibhav Srivastava <99118025+VaibhavSr007@users.noreply.github.com>
2023-06-29 09:34:33 +0530

    Time: 15 ms (79.40%), Space: 20.5 MB (18.17%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,60 +1,30 @@
 class Solution {
 public:
-    
     int trap(vector<int>& height) {
-//         int n = he.size();
-//         // left greatest element array;
-//         int maxi = INT_MIN;
-//         vector<int> l;
-//         for(int i=0; i<n; i++){
-//             maxi = max(maxi,he[i]);
-//             l.push_back(maxi);
-//         }
-//         // right greatest element array;
-//         maxi = INT_MIN;
-//         vector<int> r;
-//         for(int i=n-1; i>=0; i--){
-//             maxi = max(maxi,he[i]);
-//             r.push_back(maxi);
-//         }
-//         reverse(begin(r),end(r));
+        int n = height.size();
+        vector<int> pre(n,INT_MIN), post(n,INT_MIN);
+        int maxi = INT_MIN;
         
-//         int ans = 0;
-//         for(int i=0; i<n; i++){
-//             // cout<<l[i]<<":"<<r[i]<<" ";
-//             int min_he = min(l[i],r[i]);
-//             ans += (min_he - he[i]);
-//         }
-//         // cout<<endl;
-//         return ans;
+        for(int i=1; i<height.size(); i++){
+            maxi = max(height[i-1],maxi);
+            pre[i] = maxi;
+        }
+        
+        maxi = INT_MIN;
+        for(int i=height.size()-2; i>=0; i--){
+            maxi = max(height[i+1],maxi);
+            post[i] = maxi;
+        }
+        
+        
         
         int ans = 0;
-        int n = height.size();
-        int maxi = 0;
-        vector<int> left;  // it carrys maximum hieght at left of i
-        vector<int> right; // it carrys maximum hieght at right of i
-        for(int i=0; i<n; i++){ // for left maximum height
-            maxi = max(maxi,height[i]);
-            left.push_back(maxi);
-        }
-        maxi = 0;
-        for(int i=n-1; i>=0; i--){ // for right maximum height
-            maxi = max(maxi,height[i]);
-            right.push_back(maxi);
-        }
-        // since we iterated elements from back in height so we reverse to get correct order
-        reverse(begin(right), end(right));
-        
+
         for(int i=0; i<n; i++){
-            int min_he = min(left[i],right[i]); // since the till lowest heiht among left or right we
-                                            // can store water [3 , 1, 5] we can store wat. upto 3 only
-                                            
-            int water = min_he - height[i]; // we can only get water of height that is left above the 
-                                             // current height[i] from min_he
-            if(water >= 0){ // water can never be negative so check
-                ans += water;
+            if(pre[i] > height[i] && post[i] > height[i]){
+                int hgt = min(pre[i], post[i]);
+                ans += (hgt - height[i]);
             }
-            
         }
         
         return ans;
