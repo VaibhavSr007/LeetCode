4ce52655ae1379f29ffa6f74bb5b10b2b70bc696
Vaibhav Srivastava <99118025+VaibhavSr007@users.noreply.github.com>
2023-07-01 23:17:05 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (84.68%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
-    void solve(vector<int> op, vector<vector<int>> &ans, set<int> &s, vector<int> &nums){
-        if(op.size()==nums.size()){
-            ans.push_back(op);
+//     void solve(vector<int> op, vector<vector<int>> &ans, set<int> &s, vector<int> &nums){
+//         if(op.size()==nums.size()){
+//             ans.push_back(op);
+//         }
+        
+//         for(int i=0; i<nums.size(); i++){
+//             if(s.find(nums[i]) == s.end()){
+//                 op.push_back(nums[i]);
+//                 s.insert(nums[i]);
+//                 solve(op, ans, s, nums);
+//                 s.erase(nums[i]);
+//                 op.pop_back();
+//             }
+//         }
+//     }
+    
+    
+    void solve(int idx, vector<vector<int>> &ans, vector<int> &nums){
+    
+        if(idx == nums.size()){
+            ans.push_back(nums);
         }
         
-        for(int i=0; i<nums.size(); i++){
-            if(s.find(nums[i]) == s.end()){
-                op.push_back(nums[i]);
-                s.insert(nums[i]);
-                solve(op, ans, s, nums);
-                s.erase(nums[i]);
-                op.pop_back();
-            }
+        for(int i=idx; i<nums.size(); i++){
+            swap(nums[idx], nums[i]);
+            solve(idx+1, ans, nums);
+            swap(nums[idx], nums[i]);
         }
+        
     }
     
     
@@ -23,7 +38,8 @@ public:
         vector<int> op;
         vector<vector<int>> ans;
         set<int> s;
-        solve(op, ans, s, nums);
+        // solve(op, ans, s, nums);
+        solve(0, ans, nums);
         return ans;
     }
 };
