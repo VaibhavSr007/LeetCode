ff5c38afa4b620cd9cc43de93823dd5bc50a46a6
Vaibhav Srivastava <99118025+VaibhavSr007@users.noreply.github.com>
2023-07-03 13:52:36 +0530

    Time: 16 ms (93.93%), Space: 17.9 MB (92.27%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,30 +1,36 @@
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-        set<string> s;
+    bool isValidSudoku(vector<vector<char>>& grid) {
         
-        for(int i=0; i<9; i++){
-            for(int j=0; j<9; j++){
+        for(int x=0; x<9; x++){
+            for(int y=0; y<9; y++){
                 
-                if(board[i][j] == '.'){
-                    continue;
+                if(grid[x][y] != '.'){
+                    char c = grid[x][y];
+                    
+                    for(int i=0; i<9; i++){
+                        if(i != y && grid[x][i] == c){
+                            return false;
+                        }
+                    }
+
+                    for(int i=0; i<9; i++){
+                        if(i != x && grid[i][y] == c){
+                            return false;
+                        }
+                    }
+
+                    for(int i=(x/3)*3; i<((x/3)*3)+3; i++){
+                        for(int j=(y/3)*3; j<((y/3)*3)+3; j++){
+                            if(i!=x && y!=j && grid[i][j] == c){
+                                return false;
+                            }
+                        }
+                    }
                 }
-                
-                string row = "row"+to_string(i)+board[i][j];
-                string col = "col"+to_string(j)+board[i][j];
-                string block = "block"+to_string((i/3)*3 + (j/3))+board[i][j];
-                
-                if(s.find(row)!=s.end() || s.find(col)!=s.end() || s.find(block)!=s.end()){
-                    return false;
-                }
-                else{
-                    s.insert(row);
-                    s.insert(col);
-                    s.insert(block);
-                }
-                
-            }      
+            }
         }
+        
         return true;
     }
 };
