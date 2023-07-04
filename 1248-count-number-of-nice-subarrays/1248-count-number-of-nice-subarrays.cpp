bf6a1950efe5b830ede6b5bd19766c7e563c2997
Vaibhav Srivastava <99118025+VaibhavSr007@users.noreply.github.com>
2023-07-04 11:19:44 +0530

    Time: 363 ms (9.61%), Space: 93.5 MB (15.74%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,53 +1,13 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        // int cnt = 0;
-        // int ans = 0;
-        
-//         for(int i=0; i<nums.size(); i++){
-//             int cnt = 0;
-//             for(int j=i; j<nums.size(); j++){
-//                 if(nums[j]%2 != 0){
-//                     cnt++;
-//                 }
-                
-//                 if(cnt == k){             //brute force
-//                     ans++;
-//                 }
-//                 if(cnt > k){
-//                     break;
-//                 }
-//             }
-//         }
-        
-//         int i=0, j=0, temp=0;
-//         while(j<nums.size()){
-//             if(nums[j]%2 != 0){
-//                 cnt++;
-//                 temp  = 0;
-//             }
-            
-//             j++;
-            
-//             while(cnt == k){
-//                 if(nums[i]%2 != 0){
-//                     cnt--;
-//                 }
-//                 i++;
-//                 cout<<i<<" ";
-//                 temp++;
-//             }
-//             ans += temp;
-            
-//             // cout<<cnt<<" ";
-//         }
-//         cout<<endl;
-//         return ans;
-        
+        int ans=0, j=0, sum=0; 
         map<int,int> mp;
-        int sum = 0, ans = 0;
-        for(int i=0; i<nums.size(); i++){
-            sum += nums[i]%2;
+        while(j<nums.size()){
+            
+            sum += nums[j]%2;
+            
+            j++;
             if(sum == k){
                 ans++;
             }
@@ -55,8 +15,10 @@ public:
             if(mp.find(sum-k) != mp.end()){
                 ans += mp[sum-k];
             }
+            
             mp[sum]++;
         }
+        
         return ans;
     }
 };
