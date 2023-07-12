f9592386e59842c54b423b6331624a628ad70fbc
Vaibhav Srivastava <99118025+VaibhavSr007@users.noreply.github.com>
2023-07-12 12:28:01 +0530

    Time: 1908 ms (5.05%), Space: 367.5 MB (5.01%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,40 +1,43 @@
 class Solution {
 public:
-    bool dfs(int node, vector<int>& vis, vector<int>& Pvis, vector<vector<int>>& graph, vector<int>& chk){
-        vis[node] = 1;
-        Pvis[node] = 1;
-        chk[node] = 0;
-        for(auto i:graph[node]){
-            if(vis[i]==0){
-                if(dfs(i,vis,Pvis,graph,chk)){
-                    return true;
-                }
-            }
-            else if(Pvis[i]==1){
-                return true;
-            }
+    int solve(int idx, vector<vector<int>>& graph, vector<int> &dp, vector<int> &vis){
+        
+        if(vis[idx] == 1){
+            return 0;
         }
         
-        chk[node] = 1;
-        Pvis[node] = 0;
-        return false;
+        if(dp[idx] != -1){
+            return dp[idx];
+        }
+        
+        vis[idx] = 1;
+        int ans = 1;
+        for(auto i:graph[idx]){
+            
+            int c = solve(i, graph, dp, vis);
+            // cout<<i<<" ";
+            ans = ans & c;
+            
+        }
+        vis[idx] = 0;
+        return  dp[idx] = ans;
     }
+    
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> vis(n,0);
-        vector<int> Pvis(n,0);
-        vector<int> ans;
-        vector<int> chk(n);
+        vector<int> dp(n, -1);
+        vector<int>  ans;
+    
+        
         
         for(int i=0; i<n; i++){
-            if(vis[i]==0){
-                dfs(i,vis,Pvis,graph,chk);
+            vector<int> vis(n, 0);
+            solve(i, graph, dp, vis);
+            if(dp[i] == 1){
+                ans.push_back(i);
             }
         }
-        for(int i=0; i<n; i++){
-            if(chk[i]==1)
-                ans.push_back(i);
-        }
+        // cout<<endl;
         return ans;
     }
 };
