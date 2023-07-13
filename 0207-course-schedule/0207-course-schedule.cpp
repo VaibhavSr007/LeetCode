2439907de9890b3f7de611e2b1a3a79ec89be306
Vaibhav Srivastava <99118025+VaibhavSr007@users.noreply.github.com>
2023-07-13 12:35:10 +0530

    Time: 31 ms (22.06%), Space: 15.3 MB (13.42%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        
+        queue<int> q;
+        set<int> s;
+        map<int, vector<int>> mp;
+        vector<int> dependency(numCourses, 0);
+        
+        for(auto i:prerequisites){
+            mp[i[1]].push_back(i[0]);
+            dependency[i[0]]++;
+        }
+        
+        for(int i=0; i<numCourses; i++){
+            if(dependency[i] == 0){
+                s.insert(i);
+                q.push(i);
+            }
+        }
+        
+        
+        
+        while(!q.empty()){
+            auto temp = q.front();
+            q.pop();
+
+            for(auto i:mp[temp]){
+                dependency[i]--;
+                if(dependency[i] == 0){
+                    s.insert(i);
+                    q.push(i);
+                }
+            }
+        }
+        
+        if(s.size() != numCourses){
+            return false;
+        }
+        
+        return true;
+    }
+};
