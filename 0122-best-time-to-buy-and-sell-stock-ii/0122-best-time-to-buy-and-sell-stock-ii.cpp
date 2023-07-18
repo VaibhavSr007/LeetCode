f933c4afd4a4117c51b2438f27390e5250c687b3
Vaibhav Srivastava <99118025+VaibhavSr007@users.noreply.github.com>
2023-07-18 16:41:17 +0530

    Time: 22 ms (5.60%), Space: 17.6 MB (6.95%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,25 +1,40 @@
 class Solution {
 public:
+    int solve(int idx, int buy, vector<int> &nums, vector<vector<int>> &dp){
+        if(idx == nums.size()){
+            return 0;
+        }
+        
+        if(dp[idx][buy] != -1){
+            return dp[idx][buy];
+        }
+        
+        int ans = 0;
+        if(buy==1){
+            int pick = solve(idx+1, 0, nums, dp) - nums[idx] ;
+            int npick = solve(idx+1, buy, nums, dp);
+            ans = max(pick, npick);
+        }
+        else{
+            int pick = nums[idx]  + solve(idx+1, 1, nums, dp);
+            int npick = solve(idx+1, buy, nums, dp);
+            ans = max(pick, npick);
+        }
+        
+        // for(int i=idx+1; i<nums.size(); i++){
+        //     if(nums[i] - nums[idx] > 0)
+        //         ans = max(ans, nums[i] - nums[idx] + solve(i, idx, nums, dp));
+        // }
+        // int ans1 = solve(idx+1, idx, nums,dp);
+        
+    
+        
+        return dp[idx][buy] =  ans;
+    }
+    
     int maxProfit(vector<int>& nums) {
         int n = nums.size();
-        int i=0;
-        int maxi = 0;
-        int ans = 0;
-        for(int i=0; i<nums.size()-1; i++){
-            int st = nums[i];
-            while(i<n-1 && nums[i]<=nums[i+1]){
-                maxi = max(maxi,nums[i+1]-st);
-                i++;
-            }
-            if(i<n-1 && nums[i]>nums[i+1]){
-                ans += maxi;
-                maxi = 0;
-            }
-            if(i==n-1){
-                ans+=maxi;
-            }
-            // i--;
-        }
-        return ans;
+        vector<vector<int>> dp(n, vector<int> (2, -1));
+        return solve(0, 1, nums, dp);
     }
 };
