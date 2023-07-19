0fe6b1d43fd380742ede635f7423a235590bf0f3
Vaibhav Srivastava <99118025+VaibhavSr007@users.noreply.github.com>
2023-07-19 15:33:12 +0530

    Time: 454 ms (85.96%), Space: 89.7 MB (58.83%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<vector<int>> v,v1;
-        v1.push_back(nums.back());
-        v.push_back(nums[0]);
-        int ans1= 0, ans2=0;
         
-        for(int i=1; i<nums.size(); i++){
-            if(nums[i][0] < v.back()[1]){
-                ans1++;
+        sort(nums.begin(), nums.end());
+        vector<int> back = nums[0];
+        int i=1, cnt = 0;
+        
+        
+        
+        while(i < nums.size()){
+            if(back[1] > nums[i][0]){
+                cnt++;
             }
             else{
-                v.push_back(nums[i]);
+                back = nums[i];
             }
+            i++;
         }
         
-        
-        for(int i=nums.size()-2; i>=0; i--){
-            if(nums[i][1] > v1.back()[0]){
-                ans2++;
+        int x = 0;
+        back = nums.back();
+        i = nums.size()-2;
+        while(i >= 0){
+            if(back[0] < nums[i][1]){
+                x++;
             }
             else{
-                v1.push_back(nums[i]);
+                back = nums[i];
             }
+            i--;
         }
-        
-        return min(ans2,ans1);
+        return min(cnt,x);
     }
 };
