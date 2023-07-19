5546e90292b8d860ce7c3e15f4fc0a69cf6bd181
Vaibhav Srivastava <99118025+VaibhavSr007@users.noreply.github.com>
2023-07-19 16:22:35 +0530

    Time: 7 ms (11.58%), Space: 13.7 MB (6.99%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,22 +1,44 @@
 class Solution {
 public:
-    void solve(int i, int sum, vector<int> op, int n, vector<vector<int>> &ans){
-        if(i > 9){
-            if(sum == 0 && n==op.size()){
-                ans.push_back(op);
-            }
+//     void solve(int i, int sum, vector<int> op, int n, vector<vector<int>> &ans){
+//         if(i > 9){
+//             if(sum == 0 && n==op.size()){
+//                 ans.push_back(op);
+//             }
+//             return ;
+//         }
+        
+//         op.push_back(i);
+//         solve(i+1, sum - i, op, n, ans);
+//         op.pop_back();
+//         solve(i+1, sum, op, n, ans);
+        
+//     }
+    
+    vector<vector<int>> ans;
+    void solve(int idx, int cnt, int sum, vector<int> op){
+        
+        
+        
+        if(sum == 0 && op.size() == cnt){
+            ans.push_back(op);
             return ;
         }
         
-        op.push_back(i);
-        solve(i+1, sum - i, op, n, ans);
-        op.pop_back();
-        solve(i+1, sum, op, n, ans);
+        if(idx > 9 || sum < 0 || op.size() > cnt){
+            return ;
+        }
         
+        solve(idx+1, cnt, sum, op);
+        op.push_back(idx);
+        solve(idx+1, cnt, sum-idx, op);
+        op.pop_back();
     }
+    
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>> ans;
-        solve(1, n, {}, k, ans);
+        // vector<vector<int>> ans;
+        // solve(1, n, {}, k, ans);
+        solve(1, k, n, {});
         return ans;
     }
 };
