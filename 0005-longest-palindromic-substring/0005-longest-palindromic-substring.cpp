93f1fc005705643045873fabd496c1e4190a08f3
Vaibhav Srivastava <99118025+VaibhavSr007@users.noreply.github.com>
2023-07-21 10:30:33 +0530

    Time: 14 ms (90.73%), Space: 6.9 MB (75.05%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -6,40 +6,35 @@ public:
             return s;
         }
         
-        string ans = "";
+        int maxlen = 0, start = 0;
         for(int i=0; i<s.length()-1; i++){
             int left = i; 
             int right = i+1;
-            int cnt = 0;
-            if(s[left] == s[right]){
-                while(left >=0 && right < s.length() && s[left] == s[right]){
-                    left--;
-                    right++;
-                }
-                left++;
+            while(left >=0 && right < s.length() && s[left] == s[right]){
+                left--;
+                right++;
             }
-            right--;
-            if(ans.length() < right-left+1)
-                ans = s.substr(left, right-left+1);
-        }
-        
-        for(int i=1; i<s.length()-1; i++){
-            int left = i-1; 
-            int right = i+1;
-            if(s[left] == s[right]){
-                while(left >=0 && right < s.length() && s[left] == s[right]){
-                    left--;
-                    right++;
-                }
-                
-            }
-            left++;
-            right--;
-            if(ans.length() < right-left+1)
-                ans = s.substr(left, right-left+1);
             
+            if(maxlen < right-left-1){
+                maxlen = right-left-1;
+                start = left +1;
+            }
         }
         
-        return ans;
+        for(int i=0; i<s.length(); i++){
+            int left = i; 
+            int right = i;
+            while(left >=0 && right < s.length() && s[left] == s[right]){
+                left--;
+                right++;
+            }
+            
+            if(maxlen < right-left-1){
+                maxlen = right-left-1;
+                start = left +1;
+            }
+        }
+        
+        return s.substr(start, maxlen);
     }
 };
