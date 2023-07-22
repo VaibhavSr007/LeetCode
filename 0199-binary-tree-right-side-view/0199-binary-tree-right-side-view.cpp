b54926aa91313e62e2067e2985f3935b31de802c
Vaibhav Srivastava <99118025+VaibhavSr007@users.noreply.github.com>
2023-07-22 16:21:38 +0530

    Time: 0 ms (100.00%), Space: 12 MB (60.15%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -12,68 +12,30 @@
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        
-//         queue<TreeNode*> q;
-//         vector<int> ans;
-//         q.push(root);
-        
-//         if(root == NULL){
-//             return ans;
-//         }
-        
-//         while(!q.empty()){
-//             int n = q.size();
-            
-//             for(int i=0; i<n; i++){
-//                 auto temp = q.front();
-//                 q.pop();
-                
-//                 if(i == n-1){
-//                     ans.push_back(temp->val);
-//                 }
-                
-//                 if(temp->left){
-//                     q.push(temp->left);
-//                 }
-//                 if(temp->right){
-//                     q.push(temp->right);
-//                 }
-//             }
-//         }
-        
-//         return ans;
-        
-        if(root == NULL){
+        if(!root){
             return {};
         }
-    
-        map<int,int> mp;
-        queue<pair<TreeNode*,int>> q;
-        q.push({root,0});
-   
-        while(!q.empty()){
-            auto temp = q.front();
-            q.pop();
-       
-            int lvl = temp.second;
-       
-            // if(mp.find(lvl) == mp.end()){
-                mp[lvl] = temp.first->val;
-            // }
-       
-            if(temp.first->left){
-                q.push({temp.first->left,lvl+1});
-            }
-            if(temp.first->right){
-                q.push({temp.first->right,lvl+1});
-            }
-        }
-   
         vector<int> ans;
-        for(auto i:mp){
-            ans.push_back(i.second);
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()){
+          int n = q.size();
+
+          for(int i=0; i<n; i++){
+              auto temp = q.front();
+              q.pop();
+              if(i==n-1){
+                  ans.push_back(temp->val);
+              }
+              if(temp->left){
+                  q.push(temp->left);
+              }
+              if(temp->right){
+                  q.push(temp->right);
+              }
+          }
         }
-   
         return ans;
     }
 };
