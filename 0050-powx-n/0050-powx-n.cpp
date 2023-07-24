a79893f6df3ebd963c8f65b4cb2e580d12242469
Vaibhav Srivastava <99118025+VaibhavSr007@users.noreply.github.com>
2023-07-24 09:25:07 +0530

    Time: 0 ms (100.00%), Space: 6 MB (62.65%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,54 +1,26 @@
 class Solution {
 public:
-    
-    long double power(double x, int n){
-        long double ans;
-        if(n==0){
-            return 1;
-        }
+    double myPow(double x, int n) {
         
-        // if(n%2==0){
-        //     ans = power(x,n/2)*power(x,n/2);
-        // }
-        // else{
-        //     ans = x*power(x,n/2)*power(x,n/2);
-        // }
-        
-                // OR
-        
-        ans = x*power(x,n-1);
-        
-        
-        return ans;
-    }
-    
-    long double myPow(double x, int n) {
-//         long double ans = power(x,abs(n));
-        
-        
-//         if(n<0){
-//             ans = 1/ans;
-//         }
-        
-//         return ans;
-       
-        long double ans = 1;
+        double ans = 1;
         long long m = abs(n);
         
-        while(m>0){
+        
+        while(m > 0){
             if(m%2==0){
                 x = x*x;
-                m = m/2;
+                m = m / 2;
             }
             else{
                 ans = ans*x;
-                m = m-1;
+                m = m - 1;
             }
         }
         
-        if(n<0){
-            ans = 1/ans;
+        if(n < 0){
+            return 1/ans;
         }
+        
         return ans;
     }
 };
