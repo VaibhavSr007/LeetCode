185d47b88a6d62aa41ddc0174411211551b102ff
Vaibhav Srivastava <99118025+VaibhavSr007@users.noreply.github.com>
2023-07-27 14:06:49 +0530

    Time: 3 ms (46.16%), Space: 7.2 MB (12.71%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,31 +1,13 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
-//         vector<vector<int>> v(matrix);
-        
-//         for(int i=0; i<v.size(); i++){        // this approach uses the xtra space 
-//             for(int j=0; j<v[i].size(); j++){
-//                 matrix[i][j] = v[v.size()-1-j][i];
-//             }
-//         }
-        
-//             for(int i=0; i<matrix.size(); i++){
-//                 for(int j=0; j<i; j++){              // taking transpose of matrix
-//                     swap(matrix[i][j],matrix[j][i]);
-//                 }
-//             }          // this is more space optimized approach no xtra space needed
-        
-//             for(int i=0; i<matrix.size(); i++){
-//                 reverse(matrix[i].begin(),matrix[i].end());  // reverse each row of                                                                matrix to get desired ans
-//             }
-        
-        for(int i=0; i<matrix.size(); i++){
-            for(int j=0; j<i; j++){
-                swap(matrix[i][j],matrix[j][i]);
+    void rotate(vector<vector<int>>& nums) {
+        int n = nums.size();
+        vector<vector<int>> copy(nums);
+        for(int i=0; i<nums.size(); i++){
+            for(int j=0; j<nums.size(); j++){
+                 nums[i][j] = copy[n-j-1][i];
             }
         }
-        for(int i=0; i<matrix.size(); i++){
-            reverse(begin(matrix[i]),end(matrix[i]));
-        }
+        
     }
 };
