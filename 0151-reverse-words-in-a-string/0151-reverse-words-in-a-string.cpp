5040984e170059fc22a70caf8950d2987a3a502b
Vaibhav Srivastava <99118025+VaibhavSr007@users.noreply.github.com>
2023-07-27 11:12:02 +0530

    Time: 9 ms (36.46%), Space: 8.2 MB (43.17%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        
+        stringstream ss(s);
+        string word = "";
+        vector<string> ans;
+        
+        while(ss>>word){
+            ans.push_back(word);
+        }
+        
+        reverse(ans.begin(),ans.end());
+        string res = "";
+        for(auto i:ans){
+            res += i + " ";
+        }
+        
+        res.pop_back();
+        return res;
+    }
+};
