5ebf47986f9385fd3716cfbed29f1465d9efd866
Vaibhav Srivastava <99118025+VaibhavSr007@users.noreply.github.com>
2023-07-27 12:51:43 +0530

    Time: 8 ms (31.87%), Space: 12.1 MB (59.77%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,54 +2,7 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         
-//         int x = -1;
-//         int y;        
-
-//         //storing the index of first value that is smaller thn its next value
-//         for(int i=nums.size()-1; i>0; i--){
-//             if(nums[i-1]<nums[i]){
-//                 x = i-1;
-//                 break;
-//             }
-//         }
-// // checking if the array is already sorted in reversed manner if yes thn sort n send
-//         if(x<0){
-//             reverse(nums.begin(),nums.end());
-//         }
-// // finding the first num which is graeter thn the first value i.e smaller than its next num which is num[x] and swapping it with num[x] and reversing the part after idx x+1;
-//         else{
-//             for(int i=nums.size()-1; i>x; i--){
-//                 if(nums[i]>nums[x]){
-//                     y = i;
-//                     break;
-//                 }
-//             }
-//             swap(nums[y],nums[x]);
-//             reverse(nums.begin()+(x+1),nums.end());
-//         }  
+        next_permutation(nums.begin(),nums.end());
         
-        // next_permutation(nums.begin(),nums.end());
-        int idx = -1;
-        for(int i=nums.size()-2; i>=0; i--){
-            if(nums[i] < nums[i+1]){
-                idx  = i;
-                break;
-            }
-        }
-        
-        if(idx == -1){
-            reverse(nums.begin(),nums.end());
-        }
-        else{
-            int y;
-            for(int i=nums.size()-1; i>idx; i--){
-                if(nums[i] > nums[idx]){
-                    y = i;
-                    break;
-                }
-            }
-            swap(nums[idx],nums[y]);
-            reverse(nums.begin()+(idx+1), nums.end());
-        }
     }
 };
