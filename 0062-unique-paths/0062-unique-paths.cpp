f1c8214917d33b639dc32cb087bb2552dab56aaf
Vaibhav Srivastava <99118025+VaibhavSr007@users.noreply.github.com>
2023-09-14 11:19:55 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (8.43%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
+    vector<vector<int>> dir = {{1,0}, {0,1}};
     
-    int solve(int i, int j, int n, int m, vector<vector<int>> &dp){
-        if(i >= n || j >= m){
+    int solve(int i, int j, int m, int n, vector<vector<int>> &dp){
+        
+        if(i<0 || j<0 || i>=m || j>=n){
             return 0;
         }
-        if(i == n-1 && j == m-1){
+        
+        if(i == m-1 && j == n-1){
             return 1;
         }
         
@@ -13,15 +16,36 @@ public:
             return dp[i][j];
         }
         
-        int r = solve(i, j+1, n, m, dp);
-        int d = solve(i+1, j, n, m, dp);
+        return dp[i][j] = solve(i+1, j, m, n, dp) + solve(i, j+1, m, n, dp);
         
-        dp[i][j] = r + d;
-        return r + d;
     }
-    
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int>(n,-1));
-        return solve(0,0,m,n,dp);
+        return solve(0,0, m, n, dp);
+//         
+//         queue<vector<int>> q;
+       
+//         int ans= 0;
+        
+//         q.push({0,0});
+//         while(!q.empty()){
+//             vector<int> temp = q.front();
+//             q.pop();
+
+//             if(temp[0] == m-1 && temp[1] == n-1){
+//                 ans++;
+//             }
+
+//             for(auto d:dir){
+//                 int x = d[0] + temp[0];
+//                 int y = d[1] + temp[1];
+
+//                 if(x>=0 && y>=0 && x<m && y<n){
+//                     q.push({x,y});
+//                 }
+//             }
+//         }
+        
+//         return ans;
     }
 };
