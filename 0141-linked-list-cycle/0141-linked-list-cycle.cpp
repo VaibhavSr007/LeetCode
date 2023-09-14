eb618128b0adbf4f57ea8404e911f8c26c035867
Vaibhav Srivastava <99118025+VaibhavSr007@users.noreply.github.com>
2023-09-14 11:50:39 +0530

    Time: 7 ms (88.95%), Space: 8.5 MB (13.78%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,53 +9,17 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-//         ListNode *fast = head;
-//         ListNode *slow = head;         //  best soln  o(n)   o(n)
-        
-//         while(fast != NULL && fast->next != NULL){
-//             fast = fast->next->next;
-//             slow = slow->next;
-            
-//             if(slow == fast){
-//                 return true;
-//             }   
-//         }
-//         return false;
-        
-//         int cnt = 0;
-//         while(head!=NULL){
-//             head = head->next;
-//             cnt++;             // o(n)  o(1) soln with no extra space
-            
-//             if(cnt>10000){
-//                 return true;
-//             }
-//         }
-//         return false;
-        
-//         map<ListNode*, int> mp;
-//         ListNode* cur = head;
-//         while(cur){
-//             if(mp.find(cur) != mp.end()){        // map or set approach
-//                 cout<<cur->val<<" ";
-//                 return true;
-//             }
-//             mp[cur] = cur->val;
-//             cur = cur->next;
-//         }
-        
-//         return false;
-        
         ListNode* slow = head;
         ListNode* fast = head;
         
         while(fast && fast->next){
+            slow = slow->next; 
             fast = fast->next->next;
-            slow = slow->next;
             if(slow == fast){
                 return true;
             }
         }
+        
         return false;
     }
 };
